c1119ab034e554a853de5e9e8fdd8ff4c049fd62
Tizi-Development <108728667+TiziDevelopment@users.noreply.github.com>
2023-07-05 16:38:19 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,24 @@
 from flask import Flask, request, jsonify
-from ...googlesearch import search
+from googlesearch import search
 from duckduckgo_search import ddg
+from googleapiclient.discovery import build
 
 app = Flask(__name__)
+
+API_KEY = 'AIzaSyAd2QzPiLueo1n7UF910rQpXc3e-QmT-ZI'
+CX = '300f61ad1f18d4ea8'
+
+def google_search(query, num_results):
+    service = build('customsearch', 'v1', developerKey=API_KEY)
+    res = service.cse().list(q=query, cx=CX, num=num_results).execute()
+    return [item['link'] for item in res.get('items', [])]
 
 @app.route('/suche')
 def suche():
     keywords = request.args.get('q')
     max_results = int(request.args.get('max_results') or "5")
 
-    google_results = list(search(keywords, num=max_results))
+    google_results = google_search(keywords, num_results=max_results)
     duckduckgo_results = ddg(keywords, region='de-DE', max_results=max_results)
 
     results = {
