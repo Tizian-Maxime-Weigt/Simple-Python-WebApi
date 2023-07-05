4fd580adc8176694d8b22c4ab775e9f723a12d95
Tizi-Development <108728667+TiziDevelopment@users.noreply.github.com>
2023-07-05 16:17:54 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from googlesearch import search
 from duckduckgo_search import ddg
-app = Flask(__name__)
-    
-@app.route('/suche')
-def search():  # put application's code here
-    keywords = request.args.get('q')
-    print(request.args.get('max_results'))
-    max_results = int(request.args.get('max_results') or "3")
-    results = ddg(keywords, region='wt-wt', max_results=max_results)
-    print(results)
-    return results
 
+app = Flask(__name__)
+
+@app.route('/suche')
+def suche():
+    keywords = request.args.get('q')
+    max_results = int(request.args.get('max_results') or "5")
+
+    google_results = list(search(keywords, num=max_results))
+    duckduckgo_results = ddg(keywords, region='de-DE', max_results=max_results)
+
+    results = {
+        'Google': google_results,
+        'DuckDuckGo': duckduckgo_results
+    }
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
