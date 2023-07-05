e573e9f44c9b538ece435862e63a7eceab5aea57
Tizi-Development <108728667+Tizi-Development@users.noreply.github.com>
2023-07-06 01:20:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 
 app.route('/suche')(suche)
 
+@app.route('/')
+def index():
+    html_content = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Main Index Page</title>
+    </head>
+    <body>
+        <h1>Welcome to the Main Index Page!</h1>
+    </body>
+    </html>
+    '''
+    return render_template_string(html_content)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
