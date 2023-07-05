eb73d14939376078835a6cb594cb4591f1a6d37c
Tizi-Development <108728667+TiziDevelopment@users.noreply.github.com>
2023-07-05 16:49:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
-from api.index import search
+from api.index import suche
+
 app = Flask(__name__)
 
+app.route('/suche')(suche)
 
-app.route('/suche')(search)
+app.route('/')()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
