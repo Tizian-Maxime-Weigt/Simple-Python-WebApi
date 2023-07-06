0c6dff0e309284d043b199ee8ffb453905f0f56c
Tizi-Development <108728667+Tizi-Development@users.noreply.github.com>
2023-07-06 03:28:12 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,25 +1,8 @@
 import json
 from flask import Flask, request, jsonify
-from googleapiclient.discovery import build
 from duckduckgo_search import ddg
 
 app = Flask(__name__)
-
-API_KEY = 'AIzaSyAd2QzPiLueo1n7UF910rQpXc3e-QmT-ZI'
-CX = '300f61ad1f18d4ea8'
-
-def google_search(query, num_results):
-    service = build("customsearch", "v1", developerKey=API_KEY)
-    res = service.cse().list(q=query, cx=CX, num=num_results).execute()
-    results = []
-    for item in res['items']:
-        result = {
-            'title': item['title'],
-            'description': item['snippet'],
-            'link': item['link']
-        }
-        results.append(result)
-    return results
 
 def format_ddg_results(ddg_results):
     results = []
@@ -37,16 +20,12 @@ def suche():
     keywords = request.args.get('q')
     
     max_results = int(request.args.get('max_results', 3))
-
-    google_results = google_search(keywords, num_results=max_results)
     
     ddg_results = ddg(keywords, region='de-DE', max_results=max_results)
     
     formatted_ddg_results = format_ddg_results(ddg_results)
 
     results = {
-        
-        'Google': google_results,
         
         'DuckDuckGo': formatted_ddg_results,
         
