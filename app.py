b0d94802c2159cc95223ddb8f9be8bc5408d9600
Tizian Maxime Weigt <tizian.maxime.weigt@t-w.dev>
2025-04-02 13:16:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,22 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     html_content = '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Basic WebAPI</title>
-    </head>
-    <body>
-        <h1>Welcome to the Basic WebAPI</h1>
-        <h1>Usage:</h1>
-        <h1>/suche?q=<query>&max_res=<maximum_results></h1>
-    </body>
-    </html>
-    '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Basic WebAPI</title>
+        </head>
+        <body>
+            <h1>Welcome to the Basic WebAPI</h1>
+            <h1>Usage:</h1>
+            <h1>/suche?q=<query>&max_res=<maximum_results></h1>
+        </body>
+        </html>
+        '''
     return render_template_string(html_content)
 
 
 app.route('/suche')(suche)
-app.route('')(home)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
