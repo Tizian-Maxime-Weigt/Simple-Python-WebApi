d89fc514b8c1f0ea8b824d93f61121fc9f147968
Tizian Maxime Weigt <tizian.maxime.weigt@t-w.dev>
2025-04-02 13:12:48 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,23 +15,6 @@ def format_ddg_results(ddg_results):
         results.append(result)
     return results
 
-@app.route('/')
-def index():
-    html_content = '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Basic WebAPI</title>
-    </head>
-    <body>
-        <h1>Welcome to the Basic WebAPI</h1>
-        <h1>Usage:</h1>
-        <h1>/suche?q=<query>&max_res=<maximum_results></h1>
-    </body>
-    </html>
-    '''
-    return render_template_string(html_content)
-
 @app.route('/suche')
 def suche():
     max_results = 4
