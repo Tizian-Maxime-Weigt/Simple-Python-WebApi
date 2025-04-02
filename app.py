3d4c2c63ed989f13774e06387596b192772aab0c
Tizian Maxime Weigt <tizian.maxime.weigt@t-w.dev>
2025-04-02 13:24:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,29 @@ from flask import Flask, request, render_template_string
 from api.index import suche
 app = Flask(__name__)
 
-@app.route('/home')
-def home():
+def index():
     html_content = '''
-        <!DOCTYPE html>
+    <!DOCTYPE html>
         <html>
-        <head>
-            <title>Basic WebAPI</title>
-        </head>
-        <body>
-            <h1>Welcome to the Basic WebAPI</h1>
-            <h1>Usage:</h1>
-            <h1>/suche?q=<query>&max_res=<maximum_results></h1>
-        </body>
-        </html>
-        '''
+            <head>
+                <title>Basic WebAPI</title>
+            </head>
+            <body>
+                <h1>Welcome to the Basic WebAPI</h1>
+                <h1>Usage:</h1>
+                <h1>/suche?q=<query>&max_res=<maximum_results></h1>
+            </body>
+    </html>
+    '''
     return render_template_string(html_content)
 
+@app.route('/')
+def home():
+    return index()
+    
+@app.route('/home')
+def home():
+    return index()
 
 @app.route('/suche')
 def suche_wrapper():
