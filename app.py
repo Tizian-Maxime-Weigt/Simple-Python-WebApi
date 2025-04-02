d1435174a8f62517921f7fef95f64441f4e7a565
Tizian Maxime Weigt <tizian.maxime.weigt@t-w.dev>
2025-04-02 13:10:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,23 @@ def index():
     </html>
     '''
     return render_template_string(html_content)
-
+    
+@app.route('/home')
+def index():
+    html_content = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Basic WebAPI</title>
+    </head>
+    <body>
+        <h1>Welcome to the Basic WebAPI</h1>
+        <h1>Usage:</h1>
+        <h1>/suche?q=<query>&max_res=<maximum_results></h1>
+    </body>
+    </html>
+    '''
+    return render_template_string(html_content)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
